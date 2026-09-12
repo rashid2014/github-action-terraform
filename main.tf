2d1be54b72ad197b397c211f63f46d2ec74318c4
Harun Ur Rashid <harunnec@gmail.com>
2026-09-11 20:38:08 -0400

    Deploy to Remote

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "${var.bucket_name}-${random_id.rand.hex}"
+  bucket        = "${var.bucket_name}-${random_id.rand.hex}"
+  force_destroy = true
 }
 
 resource "random_id" "rand" {
   byte_length = 4
 }
+
+#Deploy an IAM Role with PowerUserIAMPolicy
+#Deploy an RDS Instance
